cee230162547c5a823a658f9d7602bb1b30189b9
fws407296762 <fws407296762@qq.com>
2016-11-06 23:20:44 +0800

    创建项目

---
+++ page/news/news.js
@@ -0,0 +1,146 @@
+Page({
+  data:{
+    imageUrls:[
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    newsList:[
+      {
+        "title": "朝鲜门将低级失误被证实假球，球迷调侃：还有命吗？",
+        "havePic": true,
+        "imageurls": [
+						{
+							"height": 398,
+							"width": 600,
+							"url": "http://timg01.bdimg.com/timg?pacompress&imgtype=0&sec=1439619614&di=17cb772284547877c39e3fd124826375&quality=90&size=b870_10000&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F10f813cc7abad93679e0330920420b81.jpeg"
+						},
+						{
+							"height": 358,
+							"width": 600,
+							"url": "http://timg01.bdimg.com/timg?pacompress&imgtype=0&sec=1439619614&di=1242db2b020f1e7c64c25fcc9acbda23&quality=90&size=b870_10000&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F661dd4df6f6a39a4861bac8120e42e70.jpeg"
+						},
+						{
+							"height": 396,
+							"width": 600,
+							"url": "http://timg01.bdimg.com/timg?pacompress&imgtype=0&sec=1439619614&di=190ce89c1294a4f63408a1de2c080763&quality=90&size=b870_10000&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F721f20deaab2974914dd06a46efd9c73.jpeg"
+						}
+					],
+          "desc": "日前亚足联在其官网开出了几张大罚单：朝鲜门将张柏皓被禁赛一年另缴1000美金罚款、主帅尹忠秀被禁赛一年并罚款5000美金、朝鲜足协也要被罚款20000美金。朝鲜足球给人的印象一直是纪律严明，缘何竟被亚足联开出这么大的罚单？",
+          "pubDate": "2016-11-05 17:25:58",
+          "channelName": "体育最新",
+          "nid": "3413018419949041831",
+          "source": "30年莱斯特城球迷",
+					"link": "http://baijiahao.baidu.com/s?id=1550147649903915&wfr=newsapp"
+      },
+      {
+					"pubDate": "2016-11-05 16:29:27",
+					"havePic": false,
+					"title": "湖北聋人足球队荣获全国冠军",
+					"channelName": "体育最新",
+					"imageurls": [],
+					"desc": "荆楚网消息11月4日，荣获“三菱友谊杯”残疾人民间足球争霸赛聋人乙组冠军的湖北足球健儿载誉归来，在汉举行庆功会。本次赛事采用五人赛制，共设盲人足球、聋人足球以及特奥融合足球三个组别，湖北派出一支聋人代表队参赛。决赛中，湖北代表队先后与同组的河北队、天津队、浙江队、广西队、甘肃队展开厮杀，最终脱颖而出，斩获冠军。",
+					"source": "汉网",
+					"nid": "10931477552411129180",
+          "link": "http://www.cnhan.com/app/56/378/454318.html"
+			},
+      {
+					"pubDate": "2016-11-05 16:20:46",
+					"havePic": true,
+					"title": "沪媒:博阿斯盼引进比利时国脚 孔卡或成第五外援",
+					"channelName": "体育焦点",
+					"imageurls": [
+						{
+							"height": 378,
+							"width": 570,
+							"url": "http://k.sinaimg.cn/i0/dy/slidenews/2_img/2016_29/789_1862242_197111.jpg/w570bfc.jpg"
+						}
+					],
+					"desc": "　　每一句发言，都是真真切切，毫不躲闪。昨天下午，39岁的葡萄牙人博阿斯正式出任上海上港队主教练。有着“穆里尼奥二代”之称的这位少帅相当爽气，回答提问时句句都是实在话，直言与球队的合同确实不长，就是为了夺取冠军而来。可惜的是，孔卡至少伤停七个月而前景不明。　　....",
+					"source": "新浪",
+					"channelId": "5572a108b3cdc86cf39001d4",
+					"link": "http://sports.sina.com.cn/china/j/2016-11-05/doc-ifxxneua4186862.shtml"
+			},
+      {
+					"pubDate": "2016-11-05 15:58:11",
+					"havePic": true,
+					"title": "刘爽：足球是网企业文化载体 继续“为赢而战”",
+					"channelName": "体育最新",
+					"imageurls": [
+						{
+							"height": 846,
+							"width": 1277,
+							"url": "http://img.sportscn.com/q.jpg?http://p1.ifengimg.com/a/2016_45/2374b37012229b9_size195_w1277_h846.jpg"
+						}
+					],
+					"desc": "讯、北京时间11月5日，2016网络媒体精英赛落下帷幕，网不负众望夺得冠军，完成赛事五连冠的同时也成就六冠王伟业。本场对阵小米科技的网络杯决赛意义非凡，网CEO刘爽也前来观战，再度捧得网络杯冠军，刘爽也很欣慰，很开心我们赢得了网络媒体精英赛的五连冠，足球已经成为我们企业文化的承载，我们的理念就是团结一心，为赢而战，中国互联网没有永远的王者。",
+					"source": "华体网",
+					"channelId": "5572a109b3cdc86cf39001e6",
+					"nid": "7689271433263788425",
+					"link": "http://we.sportscn.com/viewnews-2170033.html"
+			},
+      {
+					"pubDate": "2016-11-05 15:26:23",
+					"havePic": true,
+					"title": "刘爽：足球是凤凰网企业文化承载 继续“为赢而战”",
+					"channelName": "体育最新",
+					"imageurls": [
+						{
+							"height": 846,
+							"width": 1277,
+							"url": "http://p1.ifengimg.com/a/2016_45/2374b37012229b9_size195_w1277_h846.jpg"
+						},
+						{
+							"height": 846,
+							"width": 1278,
+							"url": "http://p1.ifengimg.com/a/2016_45/05551162655e390_size176_w1278_h846.jpg"
+						}
+					],
+					"desc": "北京时间11月5日，2016网络媒体精英赛落下帷幕，凤凰网不负众望夺得冠军，完成赛事五连冠的同时也成就六冠王伟业。本场对阵小米科技的网络杯决赛意义非凡，凤凰网CEO刘爽也前来观战，再度捧得网络杯冠军，刘爽也很欣慰，很开心我们赢得了网络媒体精英赛的五连冠，足球已经成为我们企业文化的承载，我们的理念就是团结一心，为赢而战，中国互联网没有永远的王者。",
+					"source": "凤凰体育",
+					"channelId": "5572a109b3cdc86cf39001e6",
+					"nid": "5118962296767295326",
+					"link": "http://sports.ifeng.com/a/20161105/50209898_0.shtml"
+			},
+      {
+					"pubDate": "2016-11-05 15:07:19",
+					"havePic": true,
+					"title": "博阿斯胡尔克在上港再聚首 来年剑指中超冠军",
+					"channelName": "国内足球最新",
+					"imageurls": [
+						{
+							"height": 424,
+							"width": 640,
+							"url": "http://img1.gtimg.com/sports/pics/hv1/42/77/2151/139888452.jpg"
+						}
+					],
+					"desc": "据新华社电4日，上海上港足球俱乐部召开新闻发布会，宣布葡萄牙名帅博阿斯成为球队新主帅，率领球队以冲冠为目标征战下一个赛季。瑞典名帅埃里克森不再担任球队主教练。博阿斯出任上港主帅上港俱乐部表示，选择博阿斯一方面是因为他出色的执教成绩，另一方面是看好他能进一步丰富球队的技战术打法，激发更多年轻球员的潜力",
+					"source": "国内足球新闻",
+					"channelId": "5572a10ab3cdc86cf39001e8",
+					"link": "http://sports.qq.com/a/20161105/017129.htm"
+				},
+        {
+					"pubDate": "2016-11-05 14:19:41",
+					"havePic": true,
+					"title": "一图读懂金球奖和世界足球先生 谁让人信服？",
+					"channelName": "国际足球最新",
+					"imageurls": [
+						{
+							"height": 353,
+							"width": 640,
+							"url": "http://img1.gtimg.com/sports/pics/hv1/46/74/2151/139887691.jpg"
+						},
+						{
+							"height": 543,
+							"width": 640,
+							"url": "http://img1.gtimg.com/sports/pics/hv1/250/73/2151/139887640.jpg"
+						}
+					],
+					"desc": "腾讯体育11月5日讯从2016年开始，《法国足球》和国际足联分开评奖，这样一来，FIFA金球奖不复存在，而两家机构分别组织世界足球先生和金球奖的评选，目前FIFA公布了竞争世界足球先生的23人名单，此前《法国足球》则公布了一份竞争金球奖的30人名单，而两处名单有什么区别，又有什么交集呢？谁是今年足坛第一人？从两份名单",
+					"source": "国际足球新闻",
+					"channelId": "5572a10ab3cdc86cf39001e7",
+					"link": "http://sports.qq.com/a/20161105/015927.htm"
+				}
+    ]
+  }
+});
---
+++ page/news/newscontent/newscontent.js
@@ -0,0 +1,3 @@
+wx.setNavigationBarTitle({
+    title:"这个是新闻标题"
+})
